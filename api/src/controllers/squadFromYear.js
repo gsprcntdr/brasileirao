0c818e34133e65a0e1e4163b51cd15594a4582de
gsprcntdr <gasparcantadori@gmail.com>
2023-10-24 20:31:46 -0300

    readme atualizado

--- api/src/controllers/squadFromYear.js
+++ api/src/controllers/squadFromYear.js
@@ -15,14 +15,36 @@ async function squadFromYear(req, res) {
       );
   }
   const rawExp = `select
-        distinct players.name, players.birthday, players.link
+        distinct players.name, players.birthday, players.link, t.id
         from players join relations r
         on r.player_id = players.id
         join teams t on r.team_id = t.id
-        where t.name ilike '%${nome}%' and r.year = ${ano};`;
+        where t.name ilike '${nome}%' and r.year = ${ano};`;
   try {
-    const selection = await knex.raw(rawExp);
-    return res.status(200).json(selection.rows);
+    const { rows } = await knex.raw(rawExp);
+
+    const teamCounts = rows.reduce((acc, item) => {
+      acc[item.id] = (acc[item.id] || 0) + 1;
+      return acc;
+    }, {});
+
+    const teams = Object.keys(teamCounts);
+
+    if (teams.length > 1) {
+      return res.status(400).json("Mais de um encontrado");
+    } else if (teams.length === 0) {
+      return res.status(400).json("Nenhum encontrado");
+    }
+
+    const format = rows.map((player) => {
+      return {
+        name: player.name,
+        birthday: player.birthday,
+        link: player.link,
+      };
+    });
+
+    return res.status(200).json(format);
   } catch (error) {
     console.log(error.message);
     return res.status(500).json(error.message);
