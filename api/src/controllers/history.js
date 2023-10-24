4f23f63a5d153fbe326236f10d8be23f8508b3bc
gsprcntdr <gasparcantadori@gmail.com>
2023-10-24 19:14:15 -0300

    get player history

--- api/src/controllers/history.js
+++ api/src/controllers/history.js
@@ -1,22 +1,116 @@
 const knex = require("../connection");
 
-async function getHistory(req, res) {
-  const { team } = req.query;
+async function getTeamHistory(req, res) {
+  let { team } = req.query;
+  if (team === undefined) {
+    return res
+      .status(400)
+      .json("Team name required: /team-history?team=[name name]");
+  }
+  if (team.indexOf(" ") !== -1) {
+    team = team.replaceAll(" ", "%");
+  }
   try {
     const rawExp = `
     select
-    distinct players.name, players.birthday, r.year
+    distinct players.name as pname, t.name as tname, players.id as pid, r.year
     from players join relations r
     on r.player_id = players.id
     join teams t on r.team_id = t.id
-    where t.name ilike '%${team}%'
+    where t.name ilike '${team}%'
     `;
-    const selection = await knex.raw(rawExp);
-    return res.status(200).json(selection.rows);
+    const { rows } = await knex.raw(rawExp);
+
+    const teamCounts = rows.reduce((acc, item) => {
+      acc[item.tname] = (acc[item.tname] || 0) + 1;
+      return acc;
+    }, {});
+
+    const teams = Object.keys(teamCounts);
+
+    if (teams.length > 1) {
+      return res.status(400).json("Multiple teams found");
+    } else if (teams.length === 0) {
+      return res.status(400).json("No matches");
+    }
+
+    const format = {
+      name: teams[0],
+      2020: [],
+      2021: [],
+      2022: [],
+      2023: [],
+    };
+
+    const years = Object.keys(format);
+    years.pop();
+
+    for (let player of rows) {
+      for (let year of years) {
+        if (player.year == year) {
+          format[year].push({
+            name: player.pname,
+            player_id: player.pid,
+          });
+          break;
+        }
+      }
+    }
+    return res.status(200).json(format);
   } catch (error) {
     console.log(error.message);
     return res.status(500).json(error.message);
   }
 }
 
-module.exports = getHistory;
+async function getPlayerHistory(req, res) {
+  let { player } = req.query;
+  if (player === undefined) {
+    return res
+      .status(400)
+      .json("Player name required: /player-history?player=[name last name]");
+  }
+  if (player.indexOf(" ") !== -1) {
+    player = player.replaceAll(" ", "%");
+  }
+  try {
+    const rawExp = `
+        select
+        distinct players.id as pid, players.name as pname, teams.name as tname, players.link, r.year
+        from players join relations r
+        on r.player_id = players.id
+        join teams on r.team_id = teams.id
+        where players.name ilike '${player}%';`;
+    const { rows } = await knex.raw(rawExp);
+
+    const idCounts = rows.reduce((acc, item) => {
+      acc[item.pid] = (acc[item.pid] || 0) + 1;
+      return acc;
+    }, {});
+
+    const ids = Object.keys(idCounts);
+
+    if (ids.length > 1) {
+      return res.status(400).json("Multiple players found");
+    } else if (ids.length === 0) {
+      return res.status(400).json("No matches");
+    }
+
+    const formattedPlayers = rows.reduce((result, obj) => {
+      const { pname, link, year, tname } = obj;
+      let playerEntry = result.find((entry) => entry.name === pname);
+      if (!playerEntry) {
+        playerEntry = { name: pname, link };
+        result.push(playerEntry);
+      }
+      playerEntry[year] = tname;
+      return result;
+    }, []);
+    return res.status(200).json(formattedPlayers);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+}
+
+module.exports = { getTeamHistory, getPlayerHistory };
--- api/src/router.js
+++ api/src/router.js
@@ -2,12 +2,13 @@ const express = require("express");
 const getPlayers = require("./controllers/players");
 const getTeams = require("./controllers/teams");
 const squadFromYear = require("./controllers/squadFromYear");
-const getHistory = require("./controllers/history");
+const { getTeamHistory, getPlayerHistory } = require("./controllers/history");
 const router = express();
 
 router.get("/players", getPlayers);
 router.get("/teams", getTeams);
 router.get("/squad", squadFromYear);
-router.get("/history", getHistory);
+router.get("/team-history", getTeamHistory);
+router.get("/player-history", getPlayerHistory);
 
 module.exports = router;
